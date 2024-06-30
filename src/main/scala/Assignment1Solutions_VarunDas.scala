232e7f548afe44afbfeb84f5806c35bb39c3dd69
varunnandandas <43205878+varunnandandas@users.noreply.github.com>
2024-06-30 12:48:52 +0530

    Assignment_1_number_1 to 20

--- src/main/scala/Assignment1Solutions_VarunDas.scala
+++ src/main/scala/Assignment1Solutions_VarunDas.scala
@@ -174,6 +174,93 @@ object Assignment1Solutions_VarunDas {
       println(s"$num12 is eligible for free shipping")
     } else println(s"$num12 is not eligible for discount or free shipping")
     println("Assignment#12 ends")
+
+    /*Assignment#13: check if given number is divisible by either 3 or 8*/
+    println("Assignment#13: check if given number is divisible by either 3 or 8")
+    println("Assignment#13: Please enter +ve integer:")
+    var num13 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num13%3==0) {
+      println(s"$num13 is divisible by 3")
+    } else if (num13%8==0) {
+      println(s"$num13 is divisible by 8")
+    } else println(s"$num13 is not divisible by either 3 or 8")
+    println("Assignment#13 ends")
+
+    /*Assignment#14: check if a given number is both non-positive and even*/
+    println("Assignment#14: check if a given number is both non-positive and even")
+    println("Assignment#14: Please enter +ve or -ve integer:")
+    var num14 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num14<0 && num14%2==0) {
+      println(s"$num14 is negative and also even number")
+    } else println(s"$num14 is not negative and also even number ")
+    println("Assignment#14 ends")
+
+    /*Assignment#15: o classify a person's age group.
+    * Classify them as a child (less than 13),
+    * teenager (between 13 and 19),
+    * and an adult (20 and above) using both logical AND and OR*/
+    println("Assignment#15: classify a person's age group")
+    println("Assignment#15: Please enter +ve integer as your age:")
+    var num15 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num15>0 && num15<13) {
+      println(s"$num15 is your age. You are classified as child.")
+    } else if ((num15 > 12) && (num15 < 20)) {
+      println(s"$num15 is your age. You are classified as teenager.")
+    } else println(s"$num15 is your age. You are classified as adult. ")
+    println("Assignment#15 ends")
+
+    /*Assignment#16: check if a given number is divisible by either 2 or 5*/
+    println("Assignment#16: check if a given number is divisible by either 2 or 5")
+    println("Assignment#16: Please enter +ve integer :")
+    var num16 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num16%2==0 && num16>0) {
+      println(s"$num16 is divisible by 2")
+    } else if (num16 % 5==0 && num16>0)  {
+      println(s"$num16 is divisible by 5.")
+    } else println(s"$num16 is not divisible by 2 or 5 or non-positive number. ")
+    println("Assignment#16 ends")
+
+    /*Assignment#17: check if a person is eligible for
+    both a senior citizen discount (age greater than 60)
+    and a student discount (age less than 25*/
+    println("Assignment#17: check if a person is eligible for \n" +
+      "both a senior citizen discount (age greater than 60) \n" +
+      "and a student discount (age less than 25)")
+    println("Assignment#17: Please enter +ve integer as your age:")
+    var num17 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num17>60) {
+      println(s"$num17 is eligible for senior citizen discount")
+    } else if (num17 <25)  {
+      println(s"$num17 is eligible for student discount.")
+    } else println(s"$num17 is your age and your are not eligible for Senior citizen discount \n" +
+    "or for student discount. ")
+    println("Assignment#17 ends")
+
+    /*Assignment#19:  check if a given number is both a multiple of 3 and 7*/
+    println("Assignment#19: check if a given number is both a multiple of 3 and 7")
+    var num19 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num19%3==0 and num19%7==0) {
+      println(s"$num19 is your number which is a multiple of both 3 and 7")
+    } else println(s"$num19 is your number which is not a multiple of both 3 and 7 ")
+    println("Assignment#19 ends")
+
+    /*Assignment#20:   check if a given number is divisible by either 5 or 9*/
+    println("Assignment#20:  check if a given number is divisible by either 5 or 9")
+    var num20 = scala.io.StdIn.readInt()
+    //var result = 0
+    if (num20%5==0) {
+      println(s"$num20 is your number which is a divisible by 5")
+    } else if (num20%9==0) {
+      println(s"$num20 is your number which is a divisible by 9")
+    } else println(s"$num20 is your number which is not divisible by 5 or 9 ")
+    println("Assignment#20 ends")
+
     /*var num =1
     for (i<- 1 to 5){
       num = 1
